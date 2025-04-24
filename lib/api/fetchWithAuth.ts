83018e2286b5bdd0c7a5bc99f4c060da8660c7df
GiorgosMarga <giorgosmarga509@gmail.com>
2025-04-24 16:34:56 +0300

    fixed cookies issue

--- lib/api/fetchWithAuth.ts
+++ lib/api/fetchWithAuth.ts
@@ -1,5 +1,6 @@
 import { StatusCodes } from "http-status-codes";
 import { refreshToken } from "./user";
+import { cookies } from "next/headers";
 
 export const fetchWithAuth = async (input: RequestInfo, init: RequestInit = {}, retry = true): Promise<Response> => {
     const res = await fetch(input, {
@@ -8,6 +9,7 @@ export const fetchWithAuth = async (input: RequestInfo, init: RequestInit = {}, 
         headers: {
             "Content-Type": "application/json",
             ...(init.headers || {}),
+             Cookie: cookies().toString(),
         },
     });
 
