6ac5b0d7890af603853f4e03543783b288ce0b7a
GiorgosMarga <giorgosmarga509@gmail.com>
2025-04-24 16:11:08 +0300

    fixed more deployment errors

--- components/otp-verification-form.tsx
+++ components/otp-verification-form.tsx
@@ -191,7 +191,9 @@ export function OtpVerificationForm() {
                         {otp.map((digit, index) => (
                             <Input
                                 key={index}
-                                ref={(el) => (inputRefs.current[index] = el)}
+                                ref={(el) => {
+                                    inputRefs.current[index] = el
+                                }}
                                 type="text"
                                 inputMode="numeric"
                                 maxLength={1}
